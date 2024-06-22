16a0a702f6e3b9df81fe42eb691d33cbd8f6a3e1
ForgeDom <maxstill2006@gmail.com>
2024-06-22 16:42:36 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int countWords(char* str) {
-	int count = 0;
-	bool word = false;
+void palidrom(char* str) {
+	int lenth = 0;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		lenth++;
+	}
 
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') {
-			word = false;
-		}
-		else if (!word) {
-			word = true;
-			count++;
+	bool isPalidrom = true;
+
+	for (int i = 0; i < lenth / 2; i++) {
+		if (str[i] != str[lenth - 1 - i]) {
+			isPalidrom = false;
+			break;
 		}
 	}
-	return count;
+
+	if (isPalidrom) {
+		cout << "The string is a palindrome." << endl;
+	}
+	else {
+		cout << "The string is not a palindrome." << endl;
+	}
 }
 int main() {
 	char str[50];
 	cin.getline(str, 50);
-	cout << countWords(str) << endl;
+	palidrom(str);
 
 	return 0;
 }
