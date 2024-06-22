3edf14309568a03a842f2e4172ecbfbf13a953ce
ForgeDom <maxstill2006@gmail.com>
2024-06-22 17:27:25 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void insertChar(char* str, char& Char1, char& Char2) {
+	int lenth = 0;
+	for (int i = 0; i < str[i] != '\0'; ++i) {
+		lenth++;
+	}
 
+	for (int i = 0; i < lenth; ++i) {
+		if (str[i] == Char2) {
+			str[i] = Char1;
+		}
+	}
+	cout << str << endl;
+}
+
+int main() {
+	char str[100];
+	char Char1 = '!';
+	char Char2 = '.';
+
+	cout << "Enter the string:" << endl;
+	cin.getline(str, 50);
+
+	insertChar(str, Char1, Char2);
 	return 0;
 }
