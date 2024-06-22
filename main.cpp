aea2e6726a05aad87707b79c74aa87e32d391e66
ForgeDom <maxstill2006@gmail.com>
2024-06-22 16:11:14 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void replace(char* str) {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] == ' ') {
-			str[i] = '\t';
-		}
-	}
-}
-
 int main() {
-	char str[] = "Hello my friend im Jhon!";
-	replace(str);
-	cout << str << endl;
+
 	return 0;
 }
