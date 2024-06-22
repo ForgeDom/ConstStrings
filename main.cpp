ae5d5098ffdce374dec340b464160baf9b76a79e
ForgeDom <maxstill2006@gmail.com>
2024-06-22 16:56:37 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void removeChar(char* str, int& number) {
+    int lenth = 0;
+    while (str[lenth] != '\0') {
+        lenth++;
+    }
 
-	return 0;
+    if (number >= 0 && number < lenth) {
+        for (int i = 0; i < lenth; ++i) {
+            if (i != number - 1) {
+                cout << str[i];
+           }
+        }
+    }
+}
+
+int main() {
+    int number;
+    char str[50];
+
+    cout << "Enter a number and a string: ";
+    cin >> number;
+    cin.ignore();
+    cin.getline(str, 50);
+
+    removeChar(str, number);
+
+    return 0;
 }
