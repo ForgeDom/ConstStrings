afbb5cb3a4b13c97504b0310553915ae516d948d
ForgeDom <maxstill2006@gmail.com>
2024-06-22 17:32:29 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int coutChar(char* str, char& Char) {
+	int lenth = 0;
+	for (int i = 0; i < str[i] != '\0'; ++i) {
+		lenth++;
+	}
 
+	int count = 0;
+
+	for (int i = 0; i < lenth; ++i) {
+		if (str[i] == Char) {
+			count++;
+		}
+	}
+	return count;
+}
+
+int main() {
+	char str[50];
+	char Char;
+
+	cout << "Enter the string:" << endl;
+	cin.getline(str, 50);
+
+	cout << "Enter the char" << endl;
+	cin >> Char;
+
+	cout << coutChar(str, Char) << endl;
 	return 0;
 }
