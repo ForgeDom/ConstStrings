3d546dddb41e82a087d31735dd1233013f4b8215
ForgeDom <maxstill2006@gmail.com>
2024-06-22 17:22:31 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void insertChar(char* str, char& Char, int& index){
+	int lenth = 0;
+	for (int i = 0; i < str[i] != '\0'; ++i) {
+		lenth++;
+	}
 
+	if (index >= 0 && index <= lenth) {
+		for (int i = lenth; i > index; --i) {
+			str[i] = str[i - 1];
+		}
+		str[index] = Char;
+		str[lenth + 1] = '\0';
+		cout << str << endl;
+	}
+}
+
+int main() {
+	char str[50];
+	char Char;
+	int index;
+
+	cout << "Enter the string:" << endl;
+	cin.getline(str, 50);
+
+	cout << "Enter the char" << endl;
+	cin >> Char;
+
+	cout << "Enter index:" << endl;
+	cin >> index;
+
+	insertChar(str, Char, index);
 	return 0;
 }
