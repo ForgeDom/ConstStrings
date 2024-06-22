a7b7559e78f7073b5c6935c529e151c27ef47026
ForgeDom <maxstill2006@gmail.com>
2024-06-22 16:22:54 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void replace(char* str) {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] == ' ') {
-			str[i] = '\t';
+void count(char* str, int& letters, int& digits, int& others) {
+	letters = 0;
+	digits = 0;
+	others = 0;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			letters++;
+		}
+		else if (str[i] >= '0' && str[i] <= '9') {
+			digits++;
+		}
+		else {
+			others++;
 		}
 	}
 }
-
 int main() {
-	char str[] = "Hello my friend im Jhon!";
-	replace(str);
-	cout << str << endl;
+	char str[50];
+	cin.getline(str, 50);
+	int letters, digits, others;
+	count(str, letters, digits, others);
+	cout << "Letters - " << letters << endl;
+	cout << "Digits - " << digits << endl;
+	cout << "Other symbols - " << others << endl;
 	return 0;
 }
