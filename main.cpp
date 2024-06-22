e05b0839235688bb7c14502f3821fe399a9e9583
ForgeDom <maxstill2006@gmail.com>
2024-06-22 17:03:02 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void remove(char* str, char& Char) {
+	int lenth = 0;
+	for (int i = 0; i < str[i] != '\0'; ++i) {
+		lenth++;
+	}
 
+	for (int i = 0; i < lenth; ++i) {
+		if (str[i] != Char) {
+			cout << str[i];
+		}
+	}
+}
+int main() {
+	char Char;
+	char str[50];
+	cin >> Char;
+	cin.ignore();
+	cin.getline(str, 50);
+	remove(str, Char);
 	return 0;
 }
